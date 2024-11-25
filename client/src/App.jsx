0c5bc71ea0a7c9a897457747d7fb5e5ff04903a5
ewen <66205491+ewenman5137@users.noreply.github.com>
2024-11-25 11:55:30 -0500

    ajout des pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Profil from "./Pages/Profil/Profil";
+import RecipePage from "./Pages/RecipePage/RecipePage";
+import RecipeProposition from "./Pages/RecipeProposition/RecipeProposition";
 
 const PrivateRoute = ({ children }) => {
   const accessToken = localStorage.getItem("access_token");
@@ -24,22 +26,10 @@ export default function App() {
       </Routes>
       <Header />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/profil"
-          element={
-            <PrivateRoute>
-              <Profil />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>}/>
+        <Route path="/profil" element={<PrivateRoute><Profil /></PrivateRoute>}/>
+        <Route path="/recipeProposition" element={<RecipeProposition />} />
+      <Route path="/recipePage/:id" element={<RecipePage />} />
       </Routes>
       <Footer />
     </>
