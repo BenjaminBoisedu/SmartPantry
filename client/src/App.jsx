9af74f1b83155d720f351b34490ff3aaaab102ad
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2024-11-25 12:26:18 -0500

    Rebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Profil from "./Pages/Profil/Profil";
