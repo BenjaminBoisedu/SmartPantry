48a9bac7abf2ca2462922d6f44c17e3b9f88f577
Gautier <gautier.fournet@gmail.com>
2024-11-26 10:20:46 -0500

    Finition Home Page

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import "./Home.css";
+import { IoIosArrowRoundDown } from "react-icons/io";
+import { RiFileListLine } from "react-icons/ri";
+import { FaLightbulb } from "react-icons/fa";
+import { FaRegBell } from "react-icons/fa";
+import { FaAngleRight, FaQ } from "react-icons/fa6";
+import { IoChevronDownSharp } from "react-icons/io5";
+import FAQ from "../../Components/ui/FAQ/faq";
 
 export default function Home() {
   return (
     <div id="home">
       <div id="header">
         <img src="/public/Img/Home/imageHome.png" alt="image" />
+
+        <div className="letscook">
+          <p>Let's Cook</p>
+          <IoIosArrowRoundDown className="arrow-icon"/>
+        </div>
       </div>
 
-      <div className="letscook">
-        <p>Let's Cook</p>
-        <p></p>
-      </div>
 
       <div className="container">
 
@@ -41,7 +49,34 @@ export default function Home() {
           <p>Our app is designed to make food management effortless. With features like inventory alerts and shopping list creation, you'll never run out of essentials. Plus, barcode scanning simplifies tracking your ingredients.</p>
         </div>
         
-        <img id="infoImage" src="/public/Img/Home/info.png" alt="" />
+        {/* <img id="infoImage" src="/public/Img/Home/info.png" alt="" /> */}
+
+        <div>
+          <div id="allCard">
+            <div id="card">
+              <FaLightbulb className="icon" />
+              <h3>Stay Update with Inventory Alerts</h3>
+              <p>Recieve timely notifications when your stock is low</p>
+            </div>
+            <div id="card">
+              <RiFileListLine className="icon" />
+              <h3>Create Custom Shopping Lists Effortlesly</h3>
+              <p>Organize your grocery needs with ease.</p>
+            </div>
+            <div id="card">
+              <FaRegBell className="icon" />
+              <h3>No Need to Think About What to Eat Anymore.</h3>
+              <p>Generate an infinite number of recipes.</p>
+            </div>
+          </div>
+          <div id="containeurBtn">
+            <a id="btnLearnMore" href="">Learn More</a>
+            <div>
+              <a id="signin" href="">Sign up</a>
+              <FaAngleRight />    
+            </div>
+          </div>
+        </div>
 
         <div id="containeurChefReviews">
           <div id="gauche">
@@ -50,7 +85,7 @@ export default function Home() {
             <img src="/public/Img/Home/fourchette.png" alt="" />
           </div>
           <div id="droite">
-            <img src="client/public/Img/Home/avis.png" alt="" />
+            <img src="/public/Img/Home/avis.png" alt="" />
           </div>
         </div>
 
@@ -58,13 +93,23 @@ export default function Home() {
           <h1>Take Controls of Your Kitchen</h1>
           <p>Sign In our app now to simplify meal planning and reduce food waste effortlessly.</p>
           <div id="containeurBtn">
-            <a id="signin" href="">Sign in</a>
+            <a id="signin" href="">Sign in</a>    
             <a id="btnLearnMore" href="">Learn More</a>
           </div>
         </div>
-      
-      </div>
 
+        <FAQ />
+
+        <div id="containeurcontrolkitchen">
+          <h1>Still have questions ?</h1>
+          <p>We're help to help you !</p> 
+          <div>
+            <a id="btnLearnMore" href="">Learn More</a>
+          </div> 
+        </div>
+      </div>
+      
     </div>
+
   );
 }
