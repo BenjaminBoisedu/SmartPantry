6fe8350f0527e95760467a785abc493f56c9e65a
Marc-Antoine Girard-Gagnon <maggagnon@etu.uqac.ca>
2024-12-03 21:54:13 -0500

    feat: two basic tests

--- client/test/setup.js
+++ client/test/setup.js
@@ -1,8 +1,41 @@
-import { afterEach } from 'vitest'
+import { afterEach, beforeEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
+import axios from 'axios';
+import { act } from 'react';
+vi.mock('axios', () =>{
+  return {
+    default: {
+      post: vi.fn(),
+      get: vi.fn()
+    }
+  }
+})
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
+})
+
+beforeEach(() =>{
+  axios.get.mockReset()
+  axios.post.mockReset()
+  const ingredientsMock = [
+    {
+      id: 0,
+      Name: "Soup",
+      Quantity: 7,
+      Unit: "gramme"
+    },
+    {
+      id: 1,
+      Name: "Soup2",
+      Quantity: 2,
+      Unit: "L"
+    }
+  ]
+
+  axios.post.mockResolvedValueOnce({
+    data: ingredientsMock,
+  });
 })
